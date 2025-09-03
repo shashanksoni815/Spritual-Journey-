e7844ab88b23c8967ac97c98ce0be9b4820bf94c
Shashank Soni <shashanksoni815@gmail.com>
2025-09-03 12:03:19 +0530

    backend

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,14 +1,19 @@
-// const asyncHandler = () => {}
+const asyncHandler = (requrstHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requrstHandler(req, res, next))
+        .catch((error) => next(error))
+    }
+}
 
 export {asyncHandler}
 
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
